381618a4ea09ded48644ecec68d76b0c9c347355
AlessandroPica <160123537+AlessandroPica@users.noreply.github.com>
2024-02-23 13:54:02 +0100

    prima versione incompleta

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    String [] array = new String[100];
+    int i = 0;
+    do{
+      Scanner input = new Scanner(System.in);
+      System.out.println("inserisci una srtinga");
+      String stringa = input.nextLine();
+      
+          
+
+
+
+      
+    }while(array[i] != "fine")
+  }
+
+}
